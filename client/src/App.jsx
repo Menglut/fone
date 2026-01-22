2024e2e3208d0256b8263bea8e286318edfa5736
Menglut <tmdwns5658@naver.com>
2026-01-22 17:27:37 +0900

    App.jsx 수정

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { ResumeFlowProvider } from "./context/ResumeFlowContext";
 
+import HomePage from "./page/HomePage"; // ✅ HomePage.jsx가 src/HomePage.jsx에 있을 때
 import ResumeInput from "./page/ResumeInput";
 import ResumeInterview from "./page/ResumeInterview";
 import ResumeResult from "./page/ResumeResult";
@@ -10,10 +11,17 @@ export default function App() {
     <ResumeFlowProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Navigate to="/resume/input" replace />} />
+          {/* ✅ 메인화면 */}
+          <Route path="/" element={<HomePage />} />
+
+          {/* ✅ 자소서 흐름 */}
+          <Route path="/resume" element={<Navigate to="/resume/input" replace />} />
           <Route path="/resume/input" element={<ResumeInput />} />
           <Route path="/resume/interview" element={<ResumeInterview />} />
           <Route path="/resume/result" element={<ResumeResult />} />
+
+          {/* ✅ 잘못된 주소는 메인으로 */}
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </BrowserRouter>
     </ResumeFlowProvider>
